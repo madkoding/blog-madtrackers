10e00883f86a945715894fea30d38244029248f6
madKoding <madkoding@gmail.com>
2024-06-07 23:39:55 -0400

    Update markdownToHtml.ts

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,10 +1,10 @@
 import { remark } from "remark";
 import html from "remark-html";
-import gridTables from "remark-grid-tables";
+//import gridTables from "remark-grid-tables";
 
 export default async function markdownToHtml(markdown: string) {
   const result = await remark()
-    .use(gridTables)
+    //.use(gridTables)
     .use(html)
     .process(markdown);
   return result.toString();
