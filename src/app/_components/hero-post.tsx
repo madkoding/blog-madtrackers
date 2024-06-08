454170ef7d65edabbcedfb258aa44df364d04a9b
madKoding <madkoding@gmail.com>
2024-06-07 23:05:10 -0400

    Updated layout

--- src/app/_components/hero-post.tsx
+++ src/app/_components/hero-post.tsx
@@ -33,13 +33,9 @@ export function HeroPost({
               {title}
             </Link>
           </h3>
-          <div className="mb-4 md:mb-0 text-lg">
-            <DateFormatter dateString={date} />
-          </div>
         </div>
         <div>
           <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-          <Avatar name={author.name} picture={author.picture} />
         </div>
       </div>
     </section>
