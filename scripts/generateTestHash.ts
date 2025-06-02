d50936eea9a7ef6ea39a3fdac8829ef3862f03b1
madKoding <madkoding@gmail.com>
2025-06-02 01:03:30 -0400

    removed old tests

--- scripts/generateTestHash.ts
+++
@@ -1,8 +0,0 @@
-import { generateUserHash } from '../src/utils/hashUtils';
-
-const username = 'test_chile';
-const hash = generateUserHash(username);
-
-console.log(`Usuario: ${username}`);
-console.log(`Hash generado: ${hash}`);
-console.log(`URL de seguimiento segura: /seguimiento/${hash}`);
